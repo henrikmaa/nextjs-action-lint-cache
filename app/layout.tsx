1937464300b3eb23db4b5bd916f509d1d3c238f6
Henrik Maaland <henrikmaa@gmail.com>
2024-10-04 09:57:20 +0200

    feat: another change to check cache

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   //This comment is extremely important to detect a change in this file
+  // another change
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
